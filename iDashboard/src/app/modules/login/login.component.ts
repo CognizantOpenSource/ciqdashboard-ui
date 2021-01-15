fc1ed39aa8e32edb5916e03141dc575d63e09a01
Shubha <shubha.acharya@cognizant.com>
2021-01-15 14:34:31 +0530

    Fix Issue in Native User Login after Review Changes

--- iDashboard/src/app/modules/login/login.component.ts
+++ iDashboard/src/app/modules/login/login.component.ts
@@ -34,7 +34,7 @@ export class LoginComponent extends UnSubscribable implements OnInit, AfterViewI
     return environment.production;
   }
   ngOnInit() {
-    this.form.type="native";
+    this.form.type="Native";
     this.managed(this.route.queryParams)
       .subscribe(params => this.returnUrl = params.returnUrl || '/home');
     this.managed(this.authService.user$).subscribe(this.success.bind(this));
